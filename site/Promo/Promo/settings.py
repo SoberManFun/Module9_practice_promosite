f9b7b3e5c9d716b2144a8a4087147294370d27c8
SoberManFun <treg3359@gmail.com>
2023-08-10 14:27:32 +0300

    concat_django_template_html_dynamic_urls_prepare_base_html

--- site/Promo/Promo/settings.py
+++ site/Promo/Promo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'visits',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ site/Promo/visits/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'title': 'Сайт для ведения промо - компаний',
+        'top_menu_username': 'Raider',
+        'top_menu_dashboard': 'Главная страница',
+        'top_menu_reports': 'Отчеты',
+        'top_menu_visits': 'ОБХОДЫ',
+        'top_menu_accounts': 'Учетные записи',
+        'top_menu_logout': 'Выйти',
+        'top_menu_settings': 'Настройки',
+        'top_menu_is_director': False,
+    }
+    return render(request, 'visits/Index.html', context)
+
+
+def visits_p(request):
+    context = {
+        'title': 'Сайт для ведения промо - компаний',
+        'top_menu_username': 'Raider',
+        'top_menu_dashboard': 'Главная страница',
+        'top_menu_reports': 'Отчеты',
+        'top_menu_visits': 'ОБХОДЫ',
+        'top_menu_accounts': 'Учетные записи',
+        'top_menu_logout': 'Выйти',
+        'top_menu_settings': 'Настройки',
+        'top_menu_is_director': False,
+        'table_column_top_name_1': 'Номер обхода',
+        'table_column_top_name_2': 'Дом',
+        'table_column_top_name_3': 'Количество квартир',
+        'table_column_top_name_4': 'Дата обхода',
+        'table_right_top_button': 'Добавить обход',
+        'table_left_but_button': 'Удалить выбранные обходы',
+        'column_right_top_name': 'Список домов',
+        'column_right_bot_button': 'Добавить новый дом',
+        'Page_list': 'Страница',
+        'visits_list': [
+            {
+                'visit_list_n': 75,
+                'house_list': 'Address_1',
+                'flats_list_quant': 3,
+                'date_list': '01.08.2023'
+
+            },
+            {
+                'visit_list_n': 48,
+                'house_list': 'Address_2',
+                'flats_list_quant': 11,
+                'date_list': '02.08.2023'
+
+            },
+            {
+                'visit_list_n': 35,
+                'house_list': 'Address_3',
+                'flats_list_quant': 17,
+                'date_list': '03.08.2023'
+
+            },
+            {
+                'visit_list_n': 91,
+                'house_list': 'Address_4',
+                'flats_list_quant': 24,
+                'date_list': '04.08.2023'
+
+            }
+        ],
+        'houses_list': ['Дом_1', 'Дом_2', 'Дом_3', 'Дом_4', 'Дом_5', 'Дом_6', 'Дом_7', 'Дом_8', 'Дом_9']
+
+    }
+    return render(request, 'visits/visits_page.html', context)
